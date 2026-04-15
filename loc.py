10e2481464648a503582261b6bf77dac6e585bef
trw <scune.de@gmail.com>
2026-04-15 17:51:59 +0200

    Added ignore folders as optional argument

--- loc.py
+++ loc.py
@@ -2,8 +2,9 @@ import sys
 import os
 
 if sys.argv[1] == "--help":
-    print("First cmdline argument can be a directory"
-          "and the other arguments can be file endings including the dot.")
+    print("First cmdline argument can be a directory path,\n"
+          "the other arguments can be file endings including the '.'\n"
+          "or folder names that should be ignored using '-i' as a prefix.")
     exit()
 
 directory = '.' # Default directoy for os.listdir()
@@ -16,21 +17,39 @@ if sys.argv[1][0] != "." or sys.argv[1][0:3] == "../":
 def GetLoc(fileName):
     with open(fileName, "r") as file:
         lines = sum(1 for line in file)
-        print(fileName, "has", lines, "of code")
+        print(fileName, "has", lines, "lines of code")
         return lines
 
 argbegin = 1
 if directory != '.':
     argbegin = 0
+
+ignoreFolders = []
+fileEndings = []
+for i in range(argbegin, len(sys.argv)):
+    if i > 0 and sys.argv[i - 1] == "-i":
+        if sys.argv[i][-1] != "/":
+            sys.argv[i] += '/'
+        ignoreFolders.append(sys.argv[i])
+    else:
+        fileEndings.append(sys.argv[i])
+
 loc = 0
 for root, dirs, files in os.walk(directory):
     for file in files:
         fullpath = os.path.join(root, file)
         dirname =  os.path.join(fullpath, file)
-        if dirname.find("/CMakeFiles/") != -1:
+
+        ignore = False
+        for ignoreFolderName in ignoreFolders:
+            if dirname.find(ignoreFolderName) != -1:
+                ignore = True
+                break;
+        if ignore:
             continue
-        for arg in sys.argv[1:]:
-            if file.endswith(arg):
+
+        for fileEnding in fileEndings:
+            if file.endswith(fileEnding):
                 loc += GetLoc(fullpath)
 
 print("Total lines of code:", loc)
